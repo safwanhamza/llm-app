6f316b316cc81101fa9c2853276a8ade7cf4fe97
safwanhamza <hamzasafwan082@gmail.com>
2025-11-04 01:03:16 +0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+// main.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	name := flag.String("name", "World", "Name to greet")
+	upper := flag.Bool("upper", false, "Print the greeting in UPPERCASE")
+	flag.Parse()
+
+	greeting := fmt.Sprintf("Hello, %s! It is %s.",
+		*name, time.Now().Format(time.RFC1123))
+
+	if *upper {
+		greeting = strings.ToUpper(greeting)
+	}
+
+	fmt.Println(greeting)
+}
